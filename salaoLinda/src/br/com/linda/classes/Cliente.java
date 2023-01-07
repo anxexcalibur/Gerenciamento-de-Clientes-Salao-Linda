d7178e2a2c524816c77374e4d17e5302004e1089
mirovesram <joaopedroverasamorim@gmail.com>
2023-01-07 19:04:03 -0300

    Necessary classes.

---
+++ salaoLinda/src/br/com/linda/classes/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.linda.classes;
+
+/**
+ *
+ * @author mimi
+ */
+public class Cliente {
+    //classe criada para metodos e atributos que serao usados pelos objetos na(a) tela(s)
+    public Cliente()
+    {
+        //construtor
+    }
+}
