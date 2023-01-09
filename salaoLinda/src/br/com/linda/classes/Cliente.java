f848465ebd3a11bba2cdf1e74dc4424a20da076f
Gabriel Galvão de Oliveira <110593619+OdisseuIII@users.noreply.github.com>
2023-01-09 19:53:09 -0300

    Implementação da classe Cliente corrigido
    Neste código, criamos uma classe chamada Cliente e uma interface chamada ClienteMtds. A classe Cliente representa um cliente de um sistema e tem atributos como nome, endereço e telefone, além de métodos para acessar e atualizar esses valores. A interface ClienteMtds define métodos que todas as classes que a implementam devem ter. A classe Cliente tem um construtor que é usado para inicializar os atributos de um objeto quando ele é criado. Além disso, ela tem métodos para adicionar pontos de fidelidade a um cliente e para atualizar a contagem de clientes fidelizados.

--- salaoLinda/src/br/com/linda/classes/Cliente.java
+++ salaoLinda/src/br/com/linda/classes/Cliente.java
@@ -10,8 +10,62 @@ package br.com.linda.classes;
  */
 public class Cliente {
     //classe criada para metodos e atributos que serao usados pelos objetos na(a) tela(s)
-    public Cliente()
+    // Atributos da classe Cliente
+    private String nome;
+    private String endereco;
+    private String telefone; 
+    private int pontosFidelidade;
+    public static int totalDeClientesFidelizados = 0;
+    
+    
+    public Cliente(String nome, String endereco,String telefone)
     {
         //construtor
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        // Inicialmente, o cliente começa com zero pontos de fidelidade
+        this.pontosFidelidade = 0;
+    }
+    // Métodos de acesso ("getters") para os atributos da classe Cliente
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getPontosFidelidade() {
+        return pontosFidelidade;
+    }
+
+    public void setPontosFidelidade(int pontosFidelidade) {
+        this.pontosFidelidade = pontosFidelidade;
+    }
+    // Método para adicionar pontos de fidelidade ao cliente
+    public void adicionarPontosFidelidade(int pontos){
+        this.pontosFidelidade += pontos;
+    }
+      // Variável estática para armazenar o total de clientes fidelizados
+    public static void IncrementarTotalDeClientesFidelizados(){
+        totalDeClientesFidelizados++;
     }
 }
