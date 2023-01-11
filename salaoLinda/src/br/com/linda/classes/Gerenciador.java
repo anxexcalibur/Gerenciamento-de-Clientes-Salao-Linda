d4e6256cdb5c862649f6a6b12009c9819d0e8cb4
mirovesram <joaopedroverasamorim@gmail.com>
2023-01-11 18:04:32 -0300

    Some changes in Gerenciador and GerenciadorSalao Classes, also the creation telas package to organization

--- salaoLinda/src/br/com/linda/classes/Gerenciador.java
+++ salaoLinda/src/br/com/linda/classes/Gerenciador.java
@@ -10,4 +10,10 @@ package br.com.linda.classes;
  */
 public interface Gerenciador {
     //Interface para colocar metodos necessarios a todas as classes filhas
+    void adicionarCliente(Cliente c);
+    void removerCliente(Cliente c);
+    Cliente pesquisarCliente(String nome);
+    void adicionarServico(Servico s);
+    void removerServico(Servico s);
+    Servico pesquisarServico(String nome);
 }
--- salaoLinda/src/br/com/linda/classes/GerenciadorSalao.java
+++ salaoLinda/src/br/com/linda/classes/GerenciadorSalao.java
@@ -4,10 +4,42 @@
  */
 package br.com.linda.classes;
 
+import java.util.List;
+
 /**
  *
  * @author mimi
  */
 public class GerenciadorSalao implements Gerenciador {
-    
+    List<Cliente> clientes;
+    List<Servico> servicos;
+    // Implementação dos métodos abstratos
+    public void adicionarCliente(Cliente c) {
+        clientes.add(c);
+    }
+    public void removerCliente(Cliente c) {
+        clientes.remove(c);
+    }
+    public Cliente pesquisarCliente(String nome) {
+        for (Cliente c : clientes) {
+            if (c.getNome().equals(nome)) {
+                return c;
+            }
+        }
+        return null;
+    }
+    public void adicionarServico(Servico s) {
+        servicos.add(s);
+    }
+    public void removerServico(Servico s) {
+        servicos.remove(s);
+    }
+    public Servico pesquisarServico(String nome) {
+        for (Servico s : servicos) {
+            if (s.getNome().equals(nome)) {
+                return s;
+            }
+        }
+        return null;
+    }
 }
