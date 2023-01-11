cb7b7fc559f809d52a5c20de7ed98425da35eed6
Mirovesaram <88739188+Mirovesaram@users.noreply.github.com>
2023-01-10 23:55:08 -0300

    123
    seriguejo

--- salaoLinda/src/br/com/linda/classes/Servico.java
+++ salaoLinda/src/br/com/linda/classes/Servico.java
@@ -9,5 +9,33 @@ package br.com.linda.classes;
  * @author mimi
  */
 public class Servico {
+    private String nome;
+    private double preco;
+    private Salao salao;
     
+    public Servico(String nome, double preco,int pontosFidelidade){
+        this.nome = nome;
+        this.preco = preco;
+    }
+    public String GetNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public double getPreco(){
+        return preco;
+    }
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+    //esse metodo serve para calcular quantos pontos o cliente ganha realizando uma compra
+     //método para calcular a quantidade de pontos de fidelidade que um cliente ganha ao realizar esse serviço
+    public void atribuirPontosFidelidade(Cliente cliente){
+        //calculando pontos de fidelidade baseado no preço do serviço
+        int pontos = (int) (this.preco / 10);
+        //regra adicional para atribuir mais pontos para serviços mais caros
+        if(this.preco > 50) pontos += 5;
+        salao.atribuirPontosFidelidade(cliente, pontos);
+    }
 }
